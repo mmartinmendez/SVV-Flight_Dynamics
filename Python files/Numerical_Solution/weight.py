28f27b33c6c046b31c1cbb2ed1409736f6b09659
Twan Keijzer <twankeijzer@hetnet.nl>
2015-03-16 11:10:15 +0100

    no I

--- Python files/Numerical_Solution/weight.py
+++ Python files/Numerical_Solution/weight.py
@@ -1,5 +1,5 @@
 import numpy as np
-def weight(W_S,W_pf,W_fuel,i):
+def weight(W_S,W_pf,W_fuel):
     W = np.zeros(len(W_fuel[0])-2)
     for i in range(len(W_fuel[0])-2):
         W[i] = (W_S+sum(W_pf)-sum(W_fuel[i+2]))
