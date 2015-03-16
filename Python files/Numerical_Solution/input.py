ac8c565a05e7728c2ab0e6f4e9e212fca2a88eae
mmartinmendez <mithun.martin.215@gmail.com>
2015-03-16 11:10:52 +0100

    new

--- Python files/Numerical_Solution/input.py
+++ Python files/Numerical_Solution/input.py
@@ -1,13 +1,17 @@
 __author__ = 'myth'
 
+from numpy import*
+
 def inputFile(filename1,filename2):
     raw_data = open(filename1, "r")
     data = (row.strip().split() for row in raw_data )
     col_data = zip(*data)
+    col_data1 = array(col_data)
 
     raw_weights = open(filename2,'r')
     weights = (row.strip().split() for row in raw_weights )
     col_weights = zip(*weights)
+    col_weights1 = array(col_weights)
 
-    return col_data , col_weights
+    return col_data1 , col_weights1
 
--- Python files/Numerical_Solution/main.py
+++ Python files/Numerical_Solution/main.py
@@ -19,9 +19,7 @@ class Main:
     def firstMeasurementSeries(self):               # Call all functions needed for calculation in the first measurement series
         print 'First Measurement Series Calculation: Begin'
         a,b,c = ISA.aparameters(self.h1)
-     #   W = weight.weight(self.W_S,self.weights,self.data[6:8])
-        lst = array(self.data[6])
-        print lst
+        W = weight.weight(self.W_S,self.weights,self.data[6],self.data[7])
 
         print 'First Measurement Series Calculation: End'
 
