1fc6c13f5884085cb24b22a03b4e7cb93717adbb
mmartinmendez <mithun.martin.215@gmail.com>
2015-03-16 12:34:38 +0100

    a

--- Python files/Numerical_Solution/input.py
+++ Python files/Numerical_Solution/input.py
@@ -9,12 +9,13 @@ def inputFile(filename1,filename2):
     col_data1 = array(col_data)
 
     raw_weights = open(filename2,'r')
-    weights = (row.strip().split() for row in raw_weights )
+    weights = (row.strip().split() for row in raw_weights)
     col_weights = zip(*weights)
     col_weights1 = array(col_weights)
-    col_weights1 = col_weights1[0]
     print col_weights1
+    col_weights2 = map(float,col_weights1[0])
+    print col_weights2
 
 
-    return col_data1 , col_weights1
+    return col_data1 , col_weights2
 
